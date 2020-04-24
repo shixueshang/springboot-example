d2117296d1425586502e045e229303f3a54747e9
shixueshang <wy_xiaoliu@126.com>
2020-04-24 12:25:33 +0800

    update projects

--- mongodb/src/main/java/com/lhd/mongodb/MongodbApplication.java
+++ mongodb/src/main/java/com/lhd/mongodb/MongodbApplication.java
@@ -2,11 +2,12 @@ package com.lhd.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by lihongde on 2018/06/15.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class MongodbApplication {
 
     public static void main(String[] args) {
--- mongodb/src/main/java/com/lhd/mongodb/model/User.java
+++ mongodb/src/main/java/com/lhd/mongodb/model/User.java
@@ -1,47 +1,29 @@
 package com.lhd.mongodb.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
  * Created by lihongde on 2018/06/15.
  */
+@Data
+@Document(collection = "user")
 public class User {
 
     @Id
     private Long id;
 
+    @Field("name")
     private String name;
 
+    @Field("age")
     private Integer age;
 
     public User(Long id, String name, Integer age){
         this.id = id;
         this.name = name;
-        this.age = age;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
         this.age = age;
     }
 }
